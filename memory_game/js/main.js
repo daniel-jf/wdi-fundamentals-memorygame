bcd6eac68daa9669cca7cb973ef963f8b548c021
Daniel J Francisco <danielfrancisco@Daniels-iMac.local>
2019-12-17 19:08:42 -0800

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,22 @@
-console.log("Up and running!");
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+
+const cards = ["queen", "queen", "king", "king"];
+const cardsInPlay = [];
+var cardOne = cards[0];
+cardsInPlay.push('cardOne');
+var cardTwo = cards[2];
+cardsInPlay.push('cardTwo');
+
+// If user flips cardOne and CardThree: You got a match!
+// If user flips cardTwo and cardFour: You got a match!
+// Otherwise: Try Again
+
 console.log("User flipped " + cardOne);
-console.log("User flipped " + cardFour);
+console.log("User flipped " + cardTwo);
+
+if (cardsInPlay === cards[0] && cards[3]) {
+	alert('You got a match!');
+} else if (cardsInPlay === cards[2] && cards[4]) {
+	alert('You got a match!');
+} else 
+	alert('Try Again!');
+
